28c49a84d1161177e617a4803ada77bb5ef4475f
Anay Sahu <66568520+hunterterms@users.noreply.github.com>
2025-10-29 12:12:18 +0530

    lab11 work

---
+++ lab11.py
@@ -0,0 +1,5 @@
+#design a python program for a univ system to manage student grades and evaluation, demostrating polymorphism: 
+#1. method overriding: create a base class 'person' with a method get_role, create derived classes "student" and "class" that override get_role.
+#2. method overloading: create a class "grade" with a method "calculate", using default argument to handle single or multiple marks.
+#3. operator overloading: create a class "score" with attribute marks, overload the plus operator using add to sum two score objects.
+#4. create objects and demostrate all types of polymorphism
